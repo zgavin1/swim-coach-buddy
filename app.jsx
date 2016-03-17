f12c6c9ddd8ce568531cbf1c2adec8dadedb7868
Zach Gavin <gavinzach@gmail.com>
2016-03-17 19:18:10 -0400

    Style completed todos based on current filter.

--- app.jsx
+++ app.jsx
@@ -280,12 +280,15 @@ class TodoApp extends Component {
         }}>Add Todo</button>
         <ul>
           {visibleTodos.map(todo=> 
-            <li key={todo.id}
+             <li key={todo.id}
                 onClick={() => {
                   store.dispatch({
                     type: "TOGGLE_TODO",
                     id: todo.id
                   });
+                }}
+                style={{
+                  textDecoration: todo.completed ? "line-through" : "none"
                 }}>
               {todo.text}
             </li>
