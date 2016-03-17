aad4f5c7ccbf8d23f2d3623b3dcd849fe50f1b99
Zach Gavin <gavinzach@gmail.com>
2016-03-17 18:24:59 -0400

    Add input field to todos.

--- app.jsx
+++ app.jsx
@@ -212,12 +212,16 @@ class TodoApp extends Component {
   render() {
     return (
       <div>
+        <input ref={node=> {
+          this.input = node;
+         }} />
         <button onClick={() => {
           store.dispatch({
             type: 'ADD_TODO',
-            text: 'Test',
+            text: this.input.value,
             id: nextTodoId++
           });
+          this.input.value = '';
         }}>Add Todo</button>
         <ul>
           {this.props.todos.map(todo=> 
