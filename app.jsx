a0a77831b5ec501689e3cdf93793db9a482782f8
Zach Gavin <gavinzach@gmail.com>
2016-03-17 22:40:05 -0400

    Magic away the FilterLink class with #connect.

--- app.jsx
+++ app.jsx
@@ -200,62 +200,84 @@ const Link = ({
   );
 };
 
-class FilterLink extends Component {
-  componentDidMount() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() =>
-      this.forceUpdate()
-    );
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    const props = this.props;
-    const { store } = this.context;
-    const state = store.getState();
-
-    return (
-      <Link
-        active={
-          props.filter === state.visibilityFilter
-        }
-        onClick={() => 
-          store.dispatch({
-            type: "SET_VISIBILITY_FILTER",
-            filter: props.filter
-          })
-        }
-      > {props.children}
-      </Link>
-    );
-  }
-}
-FilterLink.contextTypes = {
-  store: React.PropTypes.object
+const mapStateToLinkProps = (
+  state,
+  ownProps
+) => {
+  return {
+    active: ownProps.filter === state.visibilityFilter
+  };
 };
+
+const mapDispatchToLinkProps = (
+  dispatch,
+  ownProps
+) => {
+  return {
+    onClick: () => {
+      dispatch({
+        type: "SET_VISIBILITY_FILTER",
+        filter: ownProps.filter
+      });
+    }
+  };
+};
+
+const FilterLink = connect(
+  mapStateToLinkProps,
+  mapDispatchToLinkProps
+)(Link);
+
+// class FilterLink extends Component {
+//   componentDidMount() {
+//     const { store } = this.context;
+//     this.unsubscribe = store.subscribe(() =>
+//       this.forceUpdate()
+//     );
+//   }
+
+//   componentWillUnmount() {
+//     this.unsubscribe();
+//   }
+
+//   render() {
+//     const props = this.props;
+//     const { store } = this.context;
+//     const state = store.getState();
+
+//     return (
+//       <Link
+//         active={
+//           props.filter === state.visibilityFilter
+//         }
+//         onClick={() => 
+//           store.dispatch({
+//             type: "SET_VISIBILITY_FILTER",
+//             filter: props.filter
+//           })
+//         }
+//       > {props.children}
+//       </Link>
+//     );
+//   }
+// }
+// FilterLink.contextTypes = {
+//   store: React.PropTypes.object
+// };
 
 const Footer = () => (
   <p>
     Show:
     {"  "}
-    <FilterLink
-      filter="SHOW_ALL"
-    >
+    <FilterLink filter="SHOW_ALL" >
       All
     </FilterLink>
     {"  "}
-    <FilterLink
-      filter="SHOW_ACTIVE"
-    >
+    <FilterLink filter="SHOW_ACTIVE" >
       Active
     </FilterLink>
     {"  "}
-    <FilterLink
-      filter="SHOW_COMPLETED"
-    >
+    <FilterLink filter="SHOW_COMPLETED" >
       Completed
     </FilterLink>
   </p>
