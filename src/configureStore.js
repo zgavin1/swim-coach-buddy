c0c3d372560bd925a1afb90c65b51097534e3ca6
Zach Gavin <gavinzach@gmail.com>
2016-06-13 18:27:39 -0400

    Improve redux middleware to better support async promises. Currying mechanism applies steps for altering the dispatch api method.

--- src/configureStore.js
+++ src/configureStore.js
@@ -4,21 +4,30 @@ import { createStore } from 'redux';
 import rootReducer from './reducers/rootReducer';
 
 
-const addLoggingToDispatch = (store) => {
-   const rawDispatch = store.dispatch;
+const logger = (store) => (next) => {
    if (!console.group) {
-      return rawDispatch;
+      return next;
    }
 
    return (action) => {
       console.group(action.type);
       console.log('%c prev state', 'color: gray', store.getState());
       console.log('%c action', 'color: blue', action);
-      const returnValue = rawDispatch(action);
+      const returnValue = next(action);
       console.log('%c next state', 'color : green', store.getState());
       console.groupEnd(action.type);
+   };
+};
+
+// TODO:
+// should maybe understand this a little better
+// promises, store.dispatch. video 16 on egghead
+const promise = (store) => (next) => (action) => {
+   if (typeof action.then === 'function') {
+      return action.then(next);
    }
-}
+   return next(action);
+};
 
 // const persistedState = {
 //    sets: [{
@@ -29,6 +38,13 @@ const addLoggingToDispatch = (store) => {
 //       completed: "false"
 //    }]
 // }
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+   middlewares.slice().reverse().forEach(middlewares => {
+      store.dispatch = middleware(store)(store.dispatch);
+   })
+}
+
 const configureStore = () => {
    // const persistedState = loadState();
 
@@ -41,16 +57,18 @@ const configureStore = () => {
       rootReducer //,
       // persistedState
    );
+   const middlewares = [promise];
 
    if (process.env.NODE_ENV !== 'production') {
-      store.dispatch = addLoggingToDispatch(store);
+      middlewares.push(logger);
    }
-
    // store.subscribe(throttle(() => {
    //    saveState({
    //       sets: store.getState().sets
    //    });
    // }, 1000));
+
+   wrapDispatchWithMiddlewares(store, middlewares);
    
    return store;
 }
